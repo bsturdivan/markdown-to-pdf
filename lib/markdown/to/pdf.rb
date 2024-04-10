bc659e4ebb1b74abf18478bc62bf85f58e4cf612
Brian Sturdivan <bsturdivan@gmail.com>
2024-04-10 10:14:29 -0700

    first commit

---
+++ lib/markdown/to/pdf.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "pdf/version"
+
+module Markdown
+  module To
+    module Pdf
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/markdown/to/pdf/version.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module Markdown
+  module To
+    module Pdf
+      VERSION = "0.1.0"
+    end
+  end
+end
